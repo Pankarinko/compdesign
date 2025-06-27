114b330b3f1815903c8f0fc9abb7bbc90e659f78
Pankarinko <pankarinko@gmail.com>
2025-06-27 20:20:00 +0200

    Remove debug prints

--- src/main.rs
+++ src/main.rs
@@ -113,7 +113,7 @@ fn main() {
         label_brk,
         None,
     );
-    println!("{:#?}", program);
+    //println!("{:#?}", program);
     let string = args.next().unwrap().to_os_string();
     create_binary(program, temp_count, string);
 }
