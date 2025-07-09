d0ee3d70f91f5acc76fc7cb58dcc72aaee548a1d
Pankarinko <pankarinko@gmail.com>
2025-07-09 22:06:50 +0200

    Analyze liveness based on rules

--- src/liveness.rs
+++ src/liveness.rs
@@ -1,4 +1,4 @@
-use crate::ir::{IRCmd, IRExp, IRFunction};
+use crate::ir::{IRCmd, IRExp};
 
 enum Rules {
     Use(usize),
@@ -6,11 +6,59 @@ enum Rules {
     Succ(usize),
 }
 
-fn liveness_analysis() -> Vec<Vec<usize>> {
-    todo!()
+/* Creates a vector of currently live temps for every line. Repeats until saturated. */
+pub fn analyze_func_liveness(cmds: Vec<IRCmd>) -> Vec<Vec<usize>> {
+    let rules = break_func_into_rules(cmds);
+    let mut live_temps = Vec::new();
+    for _ in 0..rules.len() {
+        live_temps.push(vec![]);
+    }
+    loop {
+        if collect_live_temps(&rules, &mut live_temps) {
+            break;
+        }
+    }
+    live_temps
 }
 
-fn break_program_into_rules(funcs: Vec<IRFunction<'_>>) {}
+fn collect_live_temps(rules: &[Vec<Rules>], live_temps: &mut [Vec<usize>]) -> bool {
+    let mut saturated = true;
+    for (index, line) in rules.iter().rev().enumerate() {
+        let i = live_temps.len() - index - 1;
+        for rule in line.iter() {
+            if let Rules::Use(temp) = rule {
+                if !live_temps[i].contains(temp) {
+                    saturated = false;
+                    live_temps[i].push(*temp);
+                }
+            }
+        }
+        for l in line.iter().filter_map(|r| {
+            if let Rules::Succ(line_i) = *r {
+                Some(line_i)
+            } else {
+                None
+            }
+        }) {
+            for temp in live_temps[l].clone().iter() {
+                if !line.iter().any(|r| {
+                    if let Rules::Def(t) = r {
+                        if *t == *temp {
+                            return true;
+                        }
+                    }
+                    false
+                }) && !live_temps[i].contains(temp)
+                {
+                    saturated = false;
+                    live_temps[i].push(*temp);
+                }
+            }
+        }
+    }
+
+    saturated
+}
 
 fn break_func_into_rules(cmds: Vec<IRCmd>) -> Vec<Vec<Rules>> {
     let mut rules = Vec::new();
@@ -80,6 +128,7 @@ fn break_func_into_rules(cmds: Vec<IRCmd>) -> Vec<Vec<Rules>> {
     rules
 }
 
+/* Collects all the temps in an expression */
 fn get_temps(exp: &IRExp) -> Vec<usize> {
     match exp {
         IRExp::Temp(t) => vec![*t],
